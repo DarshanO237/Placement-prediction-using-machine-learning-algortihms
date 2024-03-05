c82bc71ea23e26a5a17db6aa49bec55e8108ec5e
DarshanO237 <99575356+DarshanO237@users.noreply.github.com>
2024-03-05 16:18:47 +0530

    Add files via upload

---
+++ placement/app.py
@@ -0,0 +1,54 @@
+import numpy as np
+import model
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__,template_folder="templates")
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+
+@app.route('/')
+def h():
+    return render_template('h.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/index')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET'])
+def predict():
+    cgpa= request.args.get('cgpa')
+    projects = request.args.get('projects')
+    workshops = request.args.get('workshops')
+    mini_projects = request.args.get('mini_projects')
+    skills = request.args.get('skills')
+    communication_skills = request.args.get('communication_skills')
+    internship = request.args.get('internship')
+    hackathon = request.args.get('hackathon')
+    tw_percentage = request.args.get('tw_percentage')
+    te_percentage = request.args.get('te_percentage')
+    backlogs = request.args.get('backlogs')
+    name=request.args.get('name')
+    s=1
+    for i in skills:
+        if i==',':
+            s=s+1
+    
+    arr = np.array([cgpa,projects,workshops,mini_projects,s,communication_skills,internship,hackathon,tw_percentage,te_percentage,backlogs])
+    
+    brr = np.asarray(arr,dtype=float)
+    output = model.predict([brr])
+
+    if(output=='Placed'):
+        out = f'Congratualations {name} !! You have high chances of getting placed!!!'
+
+    else:
+        out=f'Sorry {name} !! You have low chances of getting placed.All the best!!!!'
+    return render_template('out.html', output=out)
+     
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ placement/placement/model.py
@@ -0,0 +1,30 @@
+
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+
+df = pd.read_csv('data.csv')
+df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+df.fillna(0,inplace=True)
+x=df.drop(['StudentID','PlacementStatus'],axis=1)
+y = df['PlacementStatus']
+le = preprocessing.LabelEncoder()
+x['Internship'] = le.fit_transform(x['Internship'])
+x['Hackathon'] = le.fit_transform(x['Hackathon'])
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 100)
+
+
+
+classify= RandomForestClassifier(n_estimators= 10, criterion="entropy")
+classify.fit(x_train, y_train)
+
+
+pickle.dump(classify, open('model.pkl','wb'))
+
+model = pickle.load(open('model.pkl','rb'))
+print(classify.predict([[7.9,1,3,2,9,4.8,1,1,71,87]]))
